caa5bf18643d86016981fc28aa81748b6b74b475
Matthias Wirth <matthias.wirth@gmail.com>
2022-06-04 21:28:01 +0200

    minor update

--- install.sh
+++ install.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 set -e
 
+# GENERATE UUID
+if [ -f /boot/adsb-config.txt ]; then
+    UUID_FILE="/boot/adsbx-uuid"
+else
+    mkdir -p /usr/local/share/adsbexchange
+    UUID_FILE="/usr/local/share/adsbexchange/adsbx-uuid"
+    # move old file position
+    if [ -f /boot/adsbx-uuid ]; then
+        mv -f /boot/adsbx-uuid $UUID_FILE
+    fi
+fi
+
+function generateUUID() {
+    rm -f $UUID_FILE
+    sleep 0.$RANDOM; sleep 0.$RANDOM
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    echo New UUID: $UUID
+    echo $UUID > $UUID_FILE
+}
+
+# Check for a (valid) UUID...
+if [ -f $UUID_FILE ]; then
+    UUID=$(cat $UUID_FILE)
+    if ! [[ $UUID =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
+        # Data in UUID file is invalid.  Regenerate it!
+        echo "WARNING: Data in UUID file was invalid.  Regenerating UUID."
+        generateUUID
+    else
+        echo "Using existing valid UUID ($UUID) from $UUID_FILE"
+    fi
+else
+    # not found generate uuid and save it
+    echo "WARNING: No UUID file found, generating new UUID..."
+    generateUUID
+fi
+# GENERATE UUID END
+
 name="lbw-feed-adsbx"
 repo="https://github.com/wiedehopf/lowBandwidthFeedAdsbX"
 ipath="/usr/local/share/$name"
@@ -42,9 +79,9 @@ fi
 
 if ! [ -f $ipath/readsb ]; then
 	rm -rf /tmp/readsb &>/dev/null || true
-	git clone --single-branch --depth 1 --branch net-only https://github.com/wiedehopf/readsb.git /tmp/readsb
+	git clone --single-branch --depth 1 --branch stale https://github.com/wiedehopf/readsb.git /tmp/readsb
 	cd /tmp/readsb
-    apt install -y libncurses5-dev
+    apt install -y libncurses-dev
 	make
 	cp readsb $ipath
     cd /tmp
